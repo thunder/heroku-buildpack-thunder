e57761aa30174275ca20c46e9a2412af02de0257
Volker Killesreiter <killesre@gmail.com>
2020-09-03 14:00:55 +0200

    fix path

--- bin/util/install-sqlite.sh
+++ bin/util/install-sqlite.sh
@@ -7,7 +7,7 @@ install_sqlite() {
     mkdir -p /app/.sqlite
     curl_retry_on_18 --fail --silent --location -o /app/.sqlite/sqlite.tar.gz https://www.sqlite.org/2018/sqlite-autoconf-3260000.tar.gz
 
-    mkdir -p /app/.sqlite/php-min
+    mkdir -p /app/.sqlite/sqlite
     tar xzf /app/.sqlite/sqlite.tar.gz -C /app/.sqlite/sqlite
 
     cd sqlite
