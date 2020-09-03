3b9f96da2cd16d4e66b22a61055bb57f1af9cb74
Volker Killesreiter <killesre@gmail.com>
2020-09-03 15:05:28 +0200

    fix

--- bin/util/install-sqlite.sh
+++ bin/util/install-sqlite.sh
@@ -10,30 +10,11 @@ install_sqlite() {
     mkdir -p /app/.sqlite/sqlite
     tar xzf /app/.sqlite/sqlite.tar.gz --strip-components 1 -C /app/.sqlite/sqlite
 
-    cd /app/.sqlite/sqlite
-    ./configure --prefix=/app/.sqlite >/dev/null 2>&1
-    make
-    make install
+    /app/.sqlite/sqlite/configure --prefix=/app/.sqlite >/dev/null 2>&1
+    make -C /app/.sqlite/sqlite
+    make install -C /app/.sqlite/sqlite
     rm /app/.sqlite/sqlite.tar.gz
     rm -fr /app/.sqlite/sqlite
     ls /app/.sqlite
-
-#echo "Building SQLite…"
-#
-#
-#SOURCE_TARBALL='https://www.sqlite.org/sqlite-autoconf-3070900.tar.gz'
-#
-#curl $SOURCE_TARBALL | tar xz
-## jx
-#mv sqlite-autoconf-3070900 sqlite
-#
-#cd sqlite
-#./configure --prefix=$OUT_PREFIX
-#make
-#make install
-#
-## Cleanup
-#cd ..
-#
 
 }
