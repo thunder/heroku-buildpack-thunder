af095cd1738dd513d2c96e38d9e9bc98c983aba9
Volker Killesreiter <killesre@gmail.com>
2020-09-03 17:09:32 +0200

    push pop

--- bin/util/install-sqlite.sh
+++ bin/util/install-sqlite.sh
@@ -3,6 +3,7 @@
 install_sqlite() {
 
     status "Install sqlite"
+    pushd
 
     mkdir -p /app/.sqlite
     curl_retry_on_18 --fail --silent --location -o /app/.sqlite/sqlite.tar.gz https://www.sqlite.org/2018/sqlite-autoconf-3260000.tar.gz
@@ -16,6 +17,6 @@ install_sqlite() {
     make install
     rm /app/.sqlite/sqlite.tar.gz
     rm -fr /app/.sqlite/sqlite
-    cd /app
+    popd
 
 }
