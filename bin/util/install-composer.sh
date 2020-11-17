2ef2b765f42d01ed45fd7bec1021c75c67cef13b
Volker Killesreiter <killesre@gmail.com>
2020-11-17 10:16:13 +0100

    Switch to heroku-20 platform, use sqlite again.

--- bin/util/install-composer.sh
+++ bin/util/install-composer.sh
@@ -2,21 +2,37 @@
 
 install_minimal_composer() {
 
-    status "Install minimal composer"
+    echo "-----> Install minimal composer"
 
-    # Copied from https://github.com/heroku/heroku-buildpack-php
+    ### Copied from https://github.com/heroku/heroku-buildpack-php
+
+    # PHP expects to be installed in /app/.php/php because of compiled paths, let's set that up!
+    mkdir -p /app/.php/php/bin
+
+    # set up Composer
+    export COMPOSER_HOME=$cache_dir/.composer
+    mkdir -p $COMPOSER_HOME
+
     s3_url="https://lang-php.s3.amazonaws.com/dist-${STACK}-stable/"
+    # prepend the default repo to the list configured by the user
+    # list of repositories to use is in ascening order of precedence
+    export_env_dir "$env_dir" '^HEROKU_PHP_PLATFORM_REPOSITORIES$'
+    HEROKU_PHP_PLATFORM_REPOSITORIES="${s3_url} ${HEROKU_PHP_PLATFORM_REPOSITORIES:-}"
+    if [[ "${HEROKU_PHP_PLATFORM_REPOSITORIES}" == *" - "* ]]; then
+      # a single "-" in the user supplied string removes everything to the left of it; can be used to delete the default repo
+      echo "Default platform repository disabled."
+      HEROKU_PHP_PLATFORM_REPOSITORIES=${HEROKU_PHP_PLATFORM_REPOSITORIES#*" - "}
+      s3_url=$(echo "$HEROKU_PHP_PLATFORM_REPOSITORIES" | cut -f1 -d" " | sed 's/[^/]*$//')
+      echo "Bootstrapping using ${s3_url}..."
+    fi
 
-    mkdir -p /app/.php
-    curl_retry_on_18 --fail --silent --location -o /app/.php/php-min.tar.gz "${s3_url}php-min-7.3.14.tar.gz"
+    # minimal PHP needed for installs, and make "composer" invocations use that for now
     mkdir -p /app/.php/php-min
+
+    curl_retry_on_18 --fail --silent --location -o /app/.php/php-min.tar.gz "${s3_url}php-min-7.3.23.tar.gz"
     tar xzf /app/.php/php-min.tar.gz -C /app/.php/php-min
     rm /app/.php/php-min.tar.gz
 
-
-    curl_retry_on_18 --fail --silent --location -o /app/.php/composer.tar.gz "${s3_url}composer-1.9.2.tar.gz"
-    mkdir -p /app/.php/php
-    tar xzf /app/.php/composer.tar.gz -C /app/.php/php
-    rm /app/.php/composer.tar.gz
+    curl_retry_on_18 --fail --silent --location -o /app/.php/php/bin/composer "https://getcomposer.org/composer-stable.phar"
 
 }
