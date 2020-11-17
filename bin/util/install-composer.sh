85ca631f0b6521fa903e0d0c5bf5ba33f9d3d7d8
Volker Killesreiter <killesre@gmail.com>
2020-11-17 09:05:08 +0100

    use composer latest (2)

--- bin/util/install-composer.sh
+++ bin/util/install-composer.sh
@@ -34,7 +34,6 @@ install_minimal_composer() {
     tar xzf /app/.php/php-min.tar.gz -C /app/.php/php-min
     rm /app/.php/php-min.tar.gz
 
-    curl_retry_on_18 --fail --silent --location -o /app/.php/composer.tar.gz "${s3_url}composer-1.10.13.tar.gz"
-    tar xzf /app/.php/composer.tar.gz -C /app/.php/php
+    curl_retry_on_18 --fail --silent --location -o /app/.php/php/composer "https://getcomposer.org/composer-stable.phar"
 
 }
